4ef3d3d3890414f1d2eaa6be42e1ea2172f4ec6a
Mark Kho <markho90@gmail.com>
2018-07-08 23:08:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,9 +23,21 @@ function dec(n){
 }
 
 function makeInt(n){
+<<<<<<< HEAD
   return parseInt(n, 10)
 }
 
 function preserveDecimal(n){
   return parseFloat(n)
+=======
+  if(n === parseInt(n)){
+  return parseInt('n')
+  }
+  else if (n === parseInt(n, 10)){
+  return parseInt('n', 10)
+  }
+  else if (n === parseInt('nonsense',10)){
+  return parseInt('nonsense')
+  }
+>>>>>>> c173dd601f748d8be2f17aa65bef9a20badd570c
 }
